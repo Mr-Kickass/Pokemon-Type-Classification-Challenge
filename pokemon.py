f6046680e8a0759aaf494c42bfca7538faad8557
Mr-Kickass <ash.ps312@gmail.com>
2016-12-29 02:51:16 +0530

    Updated script

--- pokemon.py
+++ pokemon.py
@@ -17,34 +17,44 @@ from keras.layers.normalization import BatchNormalization
 
 seed=7
 np.random.seed(seed)
-dataset=np.array(pd.read_csv('C:/Users/DELL/Anaconda3/Datasets/Pokemon.csv'))
-#dataset=np.loadtxt('C:/Users/DELL/Anaconda3/Datasets/Pokemon.csv', delimiter=",")
-
-trainSet=dataset[0:560,4:11]
+df=pd.read_csv('C:/Users/DELL/Anaconda3/Datasets/Pokemon.csv')
+dataset=np.array(df)
+uniqueLabel=df['Type 1'].unique()
+#define training set and testset
+trainSet=dataset[0:801,4:11]
 testSet=dataset[560:801,4:11]
-label=dataset[:,2:3]
+compareLabel=dataset[560:801,2:3]
+#define labels = 'type 1' column of dataset 
+label=dataset[0:801,2:3]
 
+#use label encoder for encoding output
 encoder=LabelEncoder()
 encoder.fit(label)
 encoded_Y=encoder.transform(label)
 encodedLabel=np_utils.to_categorical(encoded_Y)
+print(encoded_Y.shape)
+print(encodedLabel.shape)
 
+#define model and stack layers on top
 model=Sequential()
-model.add(Dense(13,input_dim=7,init='uniform',activation='relu'))
-model.add(Dense(8,init='uniform',activation='relu'))
-
+model.add(Dense(512,input_dim=7,init='uniform',activation='relu'))
+model.add(Dense(256,init='uniform',activation='relu'))
+#attach normalization for output
 model.add(Dense(18,init='uniform'))
 model.add(BatchNormalization())
 model.add(Activation('sigmoid'))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(trainSet,encodedLabel,nb_epoch=100,batch_size=10)
+model.fit(trainSet,encodedLabel,nb_epoch=500,batch_size=10)
 
-eval=model.evaluate(trainSet,label)
+eval=model.evaluate(trainSet,encodedLabel)
 print("%s: %.2f%% " % (model.metrics_names[1], eval[1]*100))
 
-prediction=model.predict(testSet)
-
-roundOff=[round(x) for x in prediction]
-print(roundOff)
+#prediction chunk
+prediction=model.predict_classes(testSet)
+print(encoded_Y.shape)
+print(encodedLabel.shape)
+print(prediction)
+print(uniqueLabel[prediction])
+print("------------------------------------------------------")
