37559c078b4c4e8c0f49f895be1ea2bac73f2ca1
Mr-Kickass <ash.ps312@gmail.com>
2017-01-08 19:18:39 +0530

    Updated MLP
    Included in top k and argmax evaluation method
    in top k->58%
    argmax->20

--- MLP.py
+++ MLP.py
@@ -15,8 +15,8 @@ pokemonDF.head(6)
 pokemonDF.keys()
 #define hyperparameters
 learning_rate=0.001
-epoch=50
-batch_size=10
+epoch=100
+batch_size=1
 n_classes=18
 n_hidden1=512
 n_hidden2=256
@@ -42,16 +42,19 @@ for j in label:
 
 input_data_arr=(input_data_arr-input_data_arr.mean())/input_data_arr.std()
 #define training set and test set
-trainData=input_data_arr[0:600,:]
-testData=input_data_arr[601:801:,]
-trainLabel=oneHotLabel[0:600]
-testlabel=oneHotLabel[601:801]
+
+
+testData=input_data_arr[0:101]
+testlabel=np.array(oneHotLabel[0:101])
+
+trainLabel=np.array(oneHotLabel[101:801])
+trainData=input_data_arr[101:801]
     
     
 #define weights and biases for a model that consists of
 #2 hidden layers
 weights= {
-          'w1':tf.Variable(tf.random_normal([7,n_hidden1])),
+          'w1':tf.Variable(tf.random_normal([trainData.shape[1],n_hidden1])),
           'w2':tf.Variable(tf.random_normal([n_hidden1,n_hidden2])),
           'out':tf.Variable(tf.random_normal([n_hidden2,n_classes]))    
           }
@@ -72,7 +75,7 @@ out_layer=tf.matmul(layer2,weights['out'])+biases['out']
                     
 #define cost and optimizer                    
 
-costFunction=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(out_layer,trainLabel))
+costFunction=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(out_layer,y))
 #costFunction=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(out_layer,y))
 optimizer=tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(costFunction)
                     
@@ -83,13 +86,16 @@ with tf.Session() as ses:
     ses.run(init)
     for ep in range(epoch):
         avg_loss=0
-        for batch_element in range(len(input_data)):
-            _,c= ses.run([optimizer,costFunction],feed_dict={X:trainData,y:trainLabel})
+        for batch_element in range(len(trainData)):
+            _,c= ses.run([optimizer,costFunction],feed_dict={X:[trainData[batch_element]],y:[trainLabel[batch_element]]})
             avg_loss+=c
         print("Epoch "+str(ep)+" loss-> " +str(avg_loss/len(input_data)))
         
 #evaluate prediction result
-    correct_prediction=tf.equal(tf.argmax(out_layer,1),tf.argmax(testlabel,1))
-    accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+    correct_pred2 = tf.nn.in_top_k(out_layer, tf.cast(tf.argmax(y,1), "int32"), 5)
+    accuracy2 = tf.reduce_mean(tf.cast(correct_pred2, tf.float32))
+    print ("Accuracy of 'in top k' evaluation method " + str(accuracy2.eval({X:testData, y:testlabel})*100))
     
-    print("Accuracy: "+str(accuracy.eval({X:testData,y:testlabel}))*100 )
+    pred=tf.equal(tf.argmax(out_layer,1),tf.argmax(y,1))
+    accuracy=tf.reduce_mean(tf.cast(pred,tf.float32))
+    print("Accuracy of argmax method "+str(accuracy.eval({X:testData,y:testlabel})*100))
