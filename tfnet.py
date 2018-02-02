d059f52cd9f9c7ee9e55bf576afc324cdc2dd1f7
Ashwin <ash.ps312@gmail.com>
2018-02-01 22:33:46 -0800

    modified code

--- tfnet.py
+++ tfnet.py
@@ -1,14 +1,3 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fenc=utf-8
-#
-# Copyright © 2016 vishalapr <vishalapr@vishal-Lenovo-G50-70>
-#
-# Distributed under terms of the MIT license.
-
-"""
-
-"""
 
 import tensorflow as tf
 import random
